74361c20844ac107c7a39e9c73ee4e492c48ca73
cjtim <iam@cjtim.com>
2022-01-08 11:30:47 +0700

    add log to firewall (#4)
    * add log to firewall
    
    * add metadata
    
    * fix missing metadata
    
    * upgrade gcp 4.5.0
    
    * fix format
    
    * exit on format failed
    
    * separate format job
    
    * test format fail
    
    * Revert "test format fail"
    
    This reverts commit 1f37d7dc4adf3373a1d95e22020fd5af3ca68be7.
    
    * fix comment

--- firewall.tf
+++ firewall.tf
@@ -7,6 +7,16 @@ resource "google_compute_firewall" "ssh" {
   name    = "ssh"
   network = google_compute_network.vpc_network.name
 
+  dynamic "log_config" {
+    for_each = var.firewall_enable_logging == true ? [{
+      metadata = "INCLUDE_ALL_METADATA"
+    }] : []
+
+    content {
+      metadata = log_config.value.metadata
+    }
+  }
+
   allow {
     protocol = "tcp"
     ports    = ["22"]
@@ -17,6 +27,16 @@ resource "google_compute_firewall" "https" {
   name    = "https"
   network = google_compute_network.vpc_network.name
 
+  dynamic "log_config" {
+    for_each = var.firewall_enable_logging == true ? [{
+      metadata = "INCLUDE_ALL_METADATA"
+    }] : []
+
+    content {
+      metadata = log_config.value.metadata
+    }
+  }
+
   allow {
     protocol = "tcp"
     ports    = ["443"]
@@ -26,6 +46,16 @@ resource "google_compute_firewall" "https" {
 resource "google_compute_firewall" "postgres" {
   name    = "postgres"
   network = google_compute_network.vpc_network.name
+
+  dynamic "log_config" {
+    for_each = var.firewall_enable_logging == true ? [{
+      metadata = "INCLUDE_ALL_METADATA"
+    }] : []
+
+    content {
+      metadata = log_config.value.metadata
+    }
+  }
 
   allow {
     protocol = "tcp"
